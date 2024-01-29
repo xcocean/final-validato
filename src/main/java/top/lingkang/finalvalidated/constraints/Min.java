fdaaa782a4d6715a06358b775a88184dbadcf0c5
lingkang <910405656@qq.com>
2024-01-29 10:17:53 +0800

    修正文字描述的歧义

--- src/main/java/top/lingkang/finalvalidated/constraints/Min.java
+++ src/main/java/top/lingkang/finalvalidated/constraints/Min.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 /**
  * @author lingkang<br/>
  * created by 2024/1/27<br/>
- * 注解的属性最小值，小于等于<br/>
+ * 注解的属性最小值，字段大于等于此值<br/>
  * 类型：short, int, long、float、double及其包装类、BigDecimal<br/>
  * 默认返回 {字段名称} 不能小于 {value}
  */
